dc2cef89b8d02c736e1c768a85f429adf400a7c3
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2021-05-16 14:22:56 +0900

    Finish Ch08

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,29 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it "ファクトリで関連するデータを生成する" do
-    note = FactoryBot.create(:note)
-    puts "This note's project is #{note.project.inspect}"
-    puts "This note's user is #{note.user.inspect}"
-  end
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    @project = @user.projects.create(
-      name: "Test project",
-    )
-  end
+  let(:user){ FactoryBot.create(:user) }
+  let(:project){ FactoryBot.create(:project, owner: user) }
 
   it "ユーザー、プロジェクト、メッセージがあれば有効な状態であること" do
     note = Note.new(
       message: "This is a sample note.",
-      user: @user,
-      project: @project,
+      user: user,
+      project: project,
     )
 
     expect(note).to be_valid
@@ -36,32 +21,40 @@ RSpec.describe Note, type: :model do
   end
 
   describe "文字列に一致するメッセージを検索する" do
-    before do
-      @note1 = @project.notes.create(
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the first note.",
-        user: @user,
       )
+    }
 
-      @note2 = @project.notes.create(
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the second note.",
-        user: @user,
       )
+    }
 
-      @note3 = @project.notes.create(
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "First, preheat the oven.",
-        user: @user,
       )
-    end
+    }
 
     context "一致するデータが見つかるとき" do
       it "検索文字列に一致するメモを返すこと" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
 
     context "一致するデータが1件も見つからないとき" do
       it "空のコレクションを返すこと" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
